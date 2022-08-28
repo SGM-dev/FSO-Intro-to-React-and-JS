6e577d63c8a43dbd63ab26f5fecebdeae7d0045d
Sam George Mathew <sam.mathew19113@gmail.com>
2022-08-28 10:03:04 +0530

    split Total into reusable component

--- course_info/src/App.js
+++ course_info/src/App.js
@@ -10,6 +10,10 @@ const Content = (props) => {
   );
 };
 
+const Total = (props) => {
+  return <p>Number of exercises {props.total}</p>;
+};
+
 const App = () => {
   const course = "Half Stack application development";
   const part1 = "Fundamentals of React";
@@ -20,13 +24,13 @@ const App = () => {
   const exercises3 = 14;
 
   return (
-    <div>
+    <>
       <Header course={course} />
       <Content part={part1} exercises={exercises1} />
       <Content part={part2} exercises={exercises2} />
       <Content part={part3} exercises={exercises3} />
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
-    </div>
+      <Total total={exercises1 + exercises2 + exercises3} />
+    </>
   );
 };
 
