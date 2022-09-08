bf40e15a31164ee2acf006287d4fc67045d1aba1
Sam George Mathew <sam.mathew19113@gmail.com>
2022-09-08 13:19:11 +0530

    Add more statistics

--- unicafe_feedback/src/App.js
+++ unicafe_feedback/src/App.js
@@ -4,10 +4,22 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [all, setAll] = useState(0);
 
-  const handleGoodClick = () => setGood(good + 1);
-  const handleNeutralClick = () => setNeutral(neutral + 1);
-  const handleBadClick = () => setBad(bad + 1);
+  const handleGoodClick = () => {
+    setAll(all + 1);
+    setGood(good + 1);
+  };
+
+  const handleNeutralClick = () => {
+    setAll(all + 1);
+    setNeutral(neutral + 1);
+  };
+
+  const handleBadClick = () => {
+    setAll(all + 1);
+    setBad(bad + 1);
+  };
 
   return (
     <div>
@@ -19,6 +31,9 @@ const App = () => {
       <p>good: {good}</p>
       <p>neutral: {neutral}</p>
       <p>bad: {bad}</p>
+      <p>all: {all}</p>
+      <p>average: {(good - bad) / all}</p>
+      <p>Positive feedback: {good * (100/all)} % </p>
     </div>
   );
 };
